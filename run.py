45cc7e842f00ae9dabe65d34895dfb5d4b9a0d7b
Diego Ulibarri <diegoulhe@outlook.com>
2022-11-13 13:00:14 -0600

    Change requirements

--- run.py
+++ run.py
@@ -6,17 +6,24 @@ from nltk.translate.bleu_score import sentence_bleu
 
 def main():
     # Task 1
+    print("Task 1:\n")
     task1_file = open('./datasets/tiny_movie_reviews_dataset.txt', 'r')
     task1_lines = task1_file.readlines()
     for i in range(len(task1_lines)):
         print(i + 1, sentiment(task1_lines[i]))
+    
+    print("\n\n\n")
 
     # Task 2
+    print("Task 2:\n")
     ner_model = Ner
     ner_model.train(0.3)
     ner_model.plot()
+    
+    print("\n\n\n")
 
     # Task 3
+    print("Task 3:\n")
     microsoft_translator = MicrosoftTranslator()
     google_translate = GoogleTranslate()
     google_bleu_score = []
@@ -26,7 +33,7 @@ def main():
     task3_test_file = open('./datasets/europarl-v7en-es.txt', 'r')
     task3_test_lines = task3_test_file.readlines()
 
-    for i in range(len(task3_lines)):
+    for i in range(len(task3_lines)- 90):
         microsoft_translation = microsoft_translator.translate(task3_lines[i])
         google_translation = google_translate.translate(task3_lines[i])
         ref_microsoft = [microsoft_translation.split()]
